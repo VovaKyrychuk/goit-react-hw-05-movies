dab95af43acf93d60bb32332b9d2b02237f6f3f4
VovaKyrychuk <kyrychuk81@gmail.com>
2023-05-16 23:10:51 +0300

    create api.jsx

---
+++ src/pages/Home/home.jsx
@@ -0,0 +1,35 @@
+import { useEffect, useState } from 'react';
+import * as API from 'services/api';
+import TrendingList from 'components/TrendingList';
+
+export default function Home() {
+  // Створення стану для збереження списку популярних фільмів
+  const [trendingMovies, setTrendingMovies] = useState([]);
+
+  // Використання useEffect для виклику функції після монтування компонента
+  useEffect(() => {
+    // Створення асинхронної функції fetch для отримання популярних фільмів
+    const fetch = async () => {
+      try {
+        // Виклик API.searchTrending() для отримання популярних фільмів
+        const res = await API.searchTrending();
+        // Оновлення стану trendingMovies зі списком популярних фільмів
+        setTrendingMovies(res.results);
+        return;
+      } catch (error) {
+        // Обробка помилки, якщо щось пішло не так під час отримання популярних фільмів
+        console.log(error);
+      }
+    };
+    // Виклик функції fetch для отримання популярних фільмів при завантаженні компонента
+    fetch();
+  }, []); // Порожній масив залежностей означає, що ефект буде викликаний лише раз після монтування компонента
+
+  // Повернення JSX-розмітки компонента Home
+  return (
+    <>
+      <TrendingList items={trendingMovies} />{' '}
+      {/* Рендеринг компонента TrendingList з передачею списку популярних фільмів */}
+    </>
+  );
+}
---
+++ src/services/api.jsx
@@ -0,0 +1,25 @@
+import axios from 'axios';
+
+const BASE_URL = 'https://api.themoviedb.org/3';
+const API_KEY = 'cc9b631238622852f6d169b666a44c3f';
+
+axios.defaults.baseURL = BASE_URL;
+
+export const searchMovieId = async id => {
+  const res = await axios.get(`/movie/${id}?api_key=${API_KEY}`);
+  return res.data;
+};
+
+export const reviewsMovie = async id => {
+  const res = await axios.get(
+    `/movie/${id}/reviews?api_key=${API_KEY}&language=en-US&page=1`
+  );
+  return res.data;
+};
+
+export const castMovie = async id => {
+  const res = await axios.get(
+    `/movie/${id}/credits?api_key=${API_KEY}&language=en-US`
+  );
+  return res.data;
+};
